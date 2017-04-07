e6fdf0debb7496a8f10c9fa1dfbac35633856478
matiasrv <matiasrv@live.com>
2017-04-07 13:00:51 -0300

    Modifique la clase player, el metodo "dibujar", "gameover" y el "menu" en la clase juego para poder implementar 2 jugadores.

--- carita/Juego.cs
+++ carita/Juego.cs
@@ -5,9 +5,11 @@ namespace carita
 {
     class Juego
     {
+        static bool multiplayer = true;
 
         static void GameOver(ref bool _inGame)
         {
+            multiplayer = false;
             ConsoleKeyInfo salir = new ConsoleKeyInfo();
             int cont = (Console.WindowHeight - 9) / 2;
             Console.SetCursorPosition((Console.WindowWidth - 31) / 2, cont++);
@@ -28,8 +30,8 @@ namespace carita
             Console.WriteLine("██   ██  ▀█ █▀  ██▀▀▀▀  ██▀██▄");
             Console.SetCursorPosition((Console.WindowWidth - 31) / 2, cont++);
             Console.WriteLine("▀█▄▄▄█▀   ▀█▀   ██▄▄▄▄▄ ██  ██▄");
-            Console.SetCursorPosition((Console.WindowWidth - 42) / 2, cont += 2);
-            Console.WriteLine("Enter para volver a jugar o Esc para salir");
+            Console.SetCursorPosition(20, cont += 2);
+            Console.WriteLine("Enter para volver a jugar (1 jugador) / Esc para salir / Espacio para 2 jugadores");
             Console.SetCursorPosition(0, 0);
             do
             {
@@ -43,9 +45,13 @@ namespace carita
                     case ConsoleKey.Enter:
                         _inGame = true;
                         break;
+                    case ConsoleKey.Spacebar:
+                        _inGame = true;
+                        multiplayer = true;
+                        break;
                 }
 
-            } while (salir.Key != ConsoleKey.Enter && salir.Key != ConsoleKey.Escape);
+            } while (salir.Key != ConsoleKey.Enter && salir.Key != ConsoleKey.Escape && salir.Key != ConsoleKey.Spacebar);
 
         }
 
@@ -67,7 +73,8 @@ namespace carita
             Console.WriteLine(" ▀▀▄ █▄▄█ █     █  █▄▄▀ ▀▀▀ █▀▀▀  ▀▀▄ █    █▄▄█ █▄▄▀ █▀▀▀");
             Console.SetCursorPosition((Console.WindowWidth - 57) / 2, cont++);
             Console.WriteLine("▀▀▀  ▀  ▀ ▀▀▀▀ ▀▀▀ ▀  ▀ ▀▀▀ ▀▀▀▀ ▀▀▀   ▀▀▀ ▀  ▀ ▀    ▀▀▀▀");
-            Console.SetCursorPosition(0, 0);
+            Console.SetCursorPosition(Console.WindowWidth /2 - 11 , cont+= 2);
+            Console.WriteLine("ESPACIO  = 2 JUGADORES");
 
             do
             {
@@ -81,15 +88,20 @@ namespace carita
                     case ConsoleKey.Enter:
                         _inGame = true;
                         break;
+                    case ConsoleKey.Spacebar:
+                        _inGame = true;
+                        multiplayer = true;
+                        break;
                 }
 
-            } while (salir.Key != ConsoleKey.Enter && salir.Key != ConsoleKey.Escape);
+            } while (salir.Key != ConsoleKey.Enter && salir.Key != ConsoleKey.Escape && salir.Key != ConsoleKey.Spacebar);
         }
 
-        static void Dibujar(Player _player, Obstaculo[] _obstaculos, GameObject[] _enemys)
+        static void Dibujar(Player _player, Player _player2, Obstaculo[] _obstaculos, GameObject[] _enemys)
         {
             _player.Dibujar();
-
+            if (multiplayer)
+                _player2.Dibujar();
             for (int i = 0; i < _obstaculos.Length; i++)
             {
                 _obstaculos[i].Dibujar();
@@ -102,7 +114,7 @@ namespace carita
             Console.SetCursorPosition(0, 0);
         }
 
-        static void Overlap(Player _player, GameObject[] _gameObjects, GameObject[] _gameObjects2, ref bool _inGame)
+        static void Overlap(Player _player, Player _player2, GameObject[] _gameObjects, GameObject[] _gameObjects2, ref bool _inGame)
         {
             for (int i = 0; i < _gameObjects.Length; i++)
             {
@@ -120,56 +132,123 @@ namespace carita
                     _inGame = false;
                 }
             }
+            if (multiplayer)
+            {
+                for (int i = 0; i < _gameObjects.Length; i++)
+                {
+                    if ((_player2.GetX() == _gameObjects[i].GetX()
+                        && _player2.GetY() == _gameObjects[i].GetY()))
+                    {
+                        _inGame = false;
+                    }
+                }
 
-
-
+                for (int i = 0; i < _gameObjects2.Length; i++)
+                {
+                    if ((_player2.GetX() == _gameObjects2[i].GetX() && _player2.GetY() == _gameObjects2[i].GetY()))
+                    {
+                        _inGame = false;
+                    }
+                }
+            }
         }
 
         public void Play()
         {
             bool inGame = true;
             Menu(ref inGame);
-            while (inGame)
+            if(!multiplayer)
             {
-                Random rnd = new Random();
-
-                ConsoleKeyInfo flecha = new ConsoleKeyInfo();
-                Player player = new Player(Console.WindowWidth / 2, Console.WindowHeight / 2, '█');
-
-                Obstaculo[] obstaculos = new Obstaculo[10];
-                Enemy[] enemys = new Enemy[20];
-
-                for (int i = 0; i < enemys.Length; i++)
-                {
-                    enemys[i] = new Enemy(rnd.Next(0, Console.WindowWidth - 1), rnd.Next(0, Console.WindowHeight - 1), 'O');
-                }
-
-                for (int i = 0; i < obstaculos.Length; i++)
-                {
-                    obstaculos[i] = new Obstaculo(rnd.Next(0, Console.WindowWidth - 1), rnd.Next(0, Console.WindowHeight - 1), '▒');
-                }
-
                 while (inGame)
                 {
-                    Console.Clear();
-                    Console.SetCursorPosition(0, 0);
-                    Dibujar(player, obstaculos, enemys);
+                    Random rnd = new Random();
 
-                    Thread.Sleep(100);
+                    ConsoleKeyInfo flecha = new ConsoleKeyInfo();
+                    Player player = new Player(Console.WindowWidth / 2, Console.WindowHeight / 2, '█', false);
+                    Player player2 = new Player(Console.WindowWidth / 2, Console.WindowHeight / 2, '♦', false);
+
+                    Obstaculo[] obstaculos = new Obstaculo[10];
+                    Enemy[] enemys = new Enemy[20];
 
                     for (int i = 0; i < enemys.Length; i++)
-                        enemys[i].Movement();
+                    {
+                        enemys[i] = new Enemy(rnd.Next(0, Console.WindowWidth - 1), rnd.Next(0, Console.WindowHeight - 1), 'O');
+                    }
 
-                    if (Console.KeyAvailable)
-                        player.Movement(flecha);
+                    for (int i = 0; i < obstaculos.Length; i++)
+                    {
+                        obstaculos[i] = new Obstaculo(rnd.Next(0, Console.WindowWidth - 1), rnd.Next(0, Console.WindowHeight - 1), '▒');
+                    }
 
-                    Overlap(player, obstaculos, enemys, ref inGame);
+                    while (inGame)
+                    {
+                        Console.Clear();
+                        Console.SetCursorPosition(0, 0);
+                        Dibujar(player, player2, obstaculos, enemys);
 
-                    Console.Clear();
+                        Thread.Sleep(100);
+
+                        for (int i = 0; i < enemys.Length; i++)
+                            enemys[i].Movement();
+
+                        if (Console.KeyAvailable)
+                            player.Movement(flecha);
+
+                        Overlap(player, player2, obstaculos, enemys, ref inGame);
+
+                        Console.Clear();
+                    }
+                    GameOver(ref inGame);
                 }
-                GameOver(ref inGame);
-
             }
+            else
+            {
+                while (inGame)
+                {
+                    Random rnd = new Random();
+
+                    ConsoleKeyInfo flecha = new ConsoleKeyInfo();
+                    Player player = new Player(Console.WindowWidth / 2, Console.WindowHeight / 2, '█', false);
+                    Player player2 = new Player(Console.WindowWidth / 2 + 2, Console.WindowHeight / 2, '♦', true);
+
+                    Obstaculo[] obstaculos = new Obstaculo[10];
+                    Enemy[] enemys = new Enemy[20];
+
+                    for (int i = 0; i < enemys.Length; i++)
+                    {
+                        enemys[i] = new Enemy(rnd.Next(0, Console.WindowWidth - 1), rnd.Next(0, Console.WindowHeight - 1), 'O');
+                    }
+
+                    for (int i = 0; i < obstaculos.Length; i++)
+                    {
+                        obstaculos[i] = new Obstaculo(rnd.Next(0, Console.WindowWidth - 1), rnd.Next(0, Console.WindowHeight - 1), '▒');
+                    }
+
+                    while (inGame)
+                    {
+                        Console.Clear();
+                        Console.SetCursorPosition(0, 0);
+                        Dibujar(player, player2, obstaculos, enemys);
+
+                        Thread.Sleep(100);
+
+                        for (int i = 0; i < enemys.Length; i++)
+                            enemys[i].Movement();
+
+                        if (Console.KeyAvailable)
+                        {
+                            player.Movement(flecha);
+                            player2.Movement(flecha);
+                        }
+
+                        Overlap(player, player2, obstaculos, enemys, ref inGame);
+
+                        Console.Clear();
+                    }
+                    GameOver(ref inGame);
+                }
+            }
+
         }
     }
 }
--- carita/Player.cs
+++ carita/Player.cs
@@ -5,42 +5,75 @@ namespace carita
 {
     class Player : GameObject
     {
-        public Player(int _x , int _y , char _icon):base (_x,_y,_icon)
+        private bool numeroJugador = false;
+        public Player(int _x , int _y , char _icon, bool _multiplayer):base (_x,_y,_icon)
         {
-
+            numeroJugador = _multiplayer;//bool para instanciar jugador 1 o 2
         }
 
         public void Movement(ConsoleKeyInfo flecha)
         {
             flecha = Console.ReadKey();
-
-            switch (flecha.Key)
+            if (!numeroJugador)
             {
-                case ConsoleKey.UpArrow:
-                    if (y > 0)
-                    {
-                        y--;
-                    }
-                    break;
-                case ConsoleKey.DownArrow:
-                    if (y < Console.WindowHeight - 1)
-                    {
-                        y++;
-                    }
-                    break;
-                case ConsoleKey.LeftArrow:
-                    if (x > 0)
-                    {
-                        x--;
-                    }
-                    break;
-                case ConsoleKey.RightArrow:
-                    if (x < Console.WindowWidth - 1)
-                    {
-                        x++;
-                    }
-                    break;
+                switch (flecha.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (y > 0)
+                        {
+                            y--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (y < Console.WindowHeight - 1)
+                        {
+                            y++;
+                        }
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (x > 0)
+                        {
+                            x--;
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (x < Console.WindowWidth - 1)
+                        {
+                            x++;
+                        }
+                        break;
+                }
+            }else
+            {
+                switch (flecha.Key)
+                {
+                    case ConsoleKey.W:
+                        if (y > 0)
+                        {
+                            y--;
+                        }
+                        break;
+                    case ConsoleKey.S:
+                        if (y < Console.WindowHeight - 1)
+                        {
+                            y++;
+                        }
+                        break;
+                    case ConsoleKey.A:
+                        if (x > 0)
+                        {
+                            x--;
+                        }
+                        break;
+                    case ConsoleKey.D:
+                        if (x < Console.WindowWidth - 1)
+                        {
+                            x++;
+                        }
+                        break;
+                }
             }
+           
         }
     }
 }
